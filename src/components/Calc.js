f7c1380b85169e9ceeb614e8b855fb4135f8911f
yellow-detteiu <87821441+yellow-detteiu@users.noreply.github.com>
2022-05-21 15:06:10 +0900

    for文を使った機能を追加
    　for文により、n回足し算、掛け算、べき乗をすることができる機能を追加しました。

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Calc = () => {
   const [currentNum, setCurrentNum] = useState(0);
   const [inputNum, setInputNum] = useState(0);
+
+  const [forNum, setForNum] = useState(0);
+  const [forInput, setForInput] = useState(0);
+  const [forAdd, setForAdd] = useState(0);
+  const [forMulti, setForMulti] = useState(0);
+  const [forPower, setForPower] = useState(0);
+  const [forAddResult, setForAddResult] = useState(0);
+  const [forMultiResult, setForMultiResult] = useState(0);
+  const [forPowerResult, setForPowerResult] = useState(0);
 
   const onChangeInputNum = (event) => setInputNum(event.target.value);
 
@@ -41,15 +50,35 @@ const Calc = () => {
     setInputNum((1 + Math.sqrt(5)) / 2);
   };
 
+  const onChangeForNum = (event) => setForNum(event.target.value);
+  const onChangeForInput = (event) => setForInput(event.target.value);
+  const onChangeForAdd = (event) => setForAdd(event.target.value);
+  const onChangeForMulti = (event) => setForMulti(event.target.value);
+  const onChangeForPower = (event) => setForPower(event.target.value);
+
+  useEffect(() => {
+    for (let i = 0; i < forNum; i++) {
+      setForAddResult(
+        parseFloat(forInput) + parseFloat(forNum) * parseFloat(forAdd)
+      );
+      setForMultiResult(
+        parseFloat(forInput) * (parseFloat(forNum) * parseFloat(forMulti))
+      );
+      setForPowerResult(
+        parseFloat(forInput) ** (parseFloat(forPower) * parseFloat(forNum))
+      );
+    }
+  }, [forNum, forAdd, forMulti, forPower]);
+
   return (
     <React.Fragment>
-      <div class="ui huge blue header">
-        <i class="calculator icon"></i>
-        <div class="content">Result: {currentNum}</div>
+      <div className="ui huge blue header">
+        <i className="calculator icon"></i>
+        <div className="content">Result: {currentNum}</div>
       </div>
       <input
         type="number"
-        class="ui huge icon input"
+        className="ui huge icon input"
         style={{ marginRight: "20px" }}
         value={inputNum}
         onChange={onChangeInputNum}
@@ -85,6 +114,72 @@ const Calc = () => {
       <button className="ui red huge button" onClick={onGoldenRatioNum}>
         Φ
       </button>
+
+      <br />
+      <br />
+      <div className="ui huge blue header">input : </div>
+      <input
+        type="number"
+        className="ui huge icon input"
+        style={{ marginRight: "20px" }}
+        value={forInput}
+        onChange={onChangeForInput}
+        placeholder="数字を入力してください"
+      />
+      <div className="ui huge blue header">for : </div>
+      <input
+        type="number"
+        className="ui huge icon input"
+        style={{ marginRight: "20px" }}
+        value={forNum}
+        onChange={onChangeForNum}
+        placeholder="数字を入力してください"
+      />
+      <br />
+      <div className="three column row">
+        <div className="column">
+          ＋ ：
+          <input
+            type="number"
+            className="ui small icon input"
+            style={{ marginRight: "20px" }}
+            value={forAdd}
+            onChange={onChangeForAdd}
+            placeholder="数字を入力してください"
+          />
+        </div>
+        <div className="column">
+          ＊ ：
+          <input
+            type="number"
+            className="ui small icon input"
+            style={{ marginRight: "20px" }}
+            value={forMulti}
+            onChange={onChangeForMulti}
+            placeholder="数字を入力してください"
+          />
+        </div>
+        <div className="column">
+          ＾ ：
+          <input
+            type="number"
+            className="ui small icon input"
+            style={{ marginRight: "20px" }}
+            value={forPower}
+            onChange={onChangeForPower}
+            placeholder="数字を入力してください"
+          />
+        </div>
+      </div>
+      <div className="ui huge blue header">
+        <i className="calculator icon"></i>
+        <div className="three column">
+          <div className="column">ForAdd: {forAddResult}</div>
+          <div className="column">ForMulti: {forMultiResult}</div>
+          <div className="column">ForPower: {forPowerResult}</div>
+        </div>
+      </div>
+      <div></div>
     </React.Fragment>
   );
 };
